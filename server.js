f4d39c0152ef7e6de9ce013987030089c30e38d6
m-ghiringhelli <mghiring@gmail.com>
2022-12-07 10:57:47 -0800

    setup initial server / app

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./lib/app');
+const pool = require('./utils/pool');
+
+const API_URL = process.env.API_URL || 'http://localhost';
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${API_URL}:${PORT}`);
+});
+
+process.on('exit', () => {
+  console.log('Server ended');
+  pool.end();
+});
