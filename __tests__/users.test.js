b5f35cd4505b6b5265936d688a8ad617808fddc6
m-ghiringhelli <mghiring@gmail.com>
2022-12-08 10:27:50 -0800

    create initial setup.sql for users table

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,8 +1,14 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
+const app = require('../lib/app');
 
-describe('user routes', () => {
+const mockUser = {
+  email: 'test@test.com',
+  password: '123456',
+}
+
+describe('user tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -11,7 +17,12 @@ describe('user routes', () => {
     pool.end();
   });
 
-  it('POST to users should create a user', () => {
-    expect(1).toEqual(1);
+  it('POST to users should create a user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: mockUser.email,
+    })
   })
 });
