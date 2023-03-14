09b6f119d44e70dfae4bfe4fdd04b28423b68428
m-ghiringhelli <mghiring@gmail.com>
2023-03-14 12:13:05 -0700

    successfully fetch album by id

--- __tests__/albums.test.js
+++ __tests__/albums.test.js
@@ -12,10 +12,29 @@ describe('album tests', () => {
     pool.end();
   });
 
-  it.only('GET from /albums should return the list of albums', async () => {
+  it('GET from /albums should return the list of albums', async () => {
     const res = await request(app).get('/api/v1/albums');
 
     expect(res.status).toBe(200);
     expect(res.body).toHaveLength(220);
+  });
+
+  it('GET from /albums/:id should return correct album', async () => {
+    const res = await request(app).get('/api/v1/albums/3');
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: '3',
+      artist: 'Grandmaster Flash and the Furious Five',
+      title: 'The Message',
+      released: '10/1/1982',
+      location: 'The Bronx, NY',
+      recordCompany: 'Sugar Hill',
+      region: 'East',
+    });
+
+    const noAlbum = await request(app).get('/api/v1/albums/300');
+    expect(noAlbum.status).toBe(200);
+    expect(noAlbum.body).toEqual(null);
   });
 });
